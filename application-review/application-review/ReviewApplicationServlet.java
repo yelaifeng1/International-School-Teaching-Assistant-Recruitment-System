50b6bddb67f64e145b85c37f7ae4fae81c55d30e
Wjx0723 <2021213042@bupt.cn>
2026-03-25 06:32:41 +0800

    add basic review application servlet

---
+++ application-review/application-review/ReviewApplicationServlet.java
@@ -0,0 +1,6 @@
+public class ReviewApplicationServlet {
+    public void reviewApplication(int id, String status) {
+        System.out.println("Application ID: " + id);
+        System.out.println("Updated status: " + status);
+    }
+}
