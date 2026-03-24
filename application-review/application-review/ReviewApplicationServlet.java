666b1f3175afd908aa7361a4c9551d891dfdc2f0
Wjx0723 <2021213042@bupt.cn>
2026-03-25 06:42:19 +0800

    Delete application-review/application-review/ReviewApplicationServlet.java

--- application-review/application-review/ReviewApplicationServlet.java
+++
@@ -1,6 +0,0 @@
-public class ReviewApplicationServlet {
-    public void reviewApplication(int id, String status) {
-        System.out.println("Application ID: " + id);
-        System.out.println("Updated status: " + status);
-    }
-}
